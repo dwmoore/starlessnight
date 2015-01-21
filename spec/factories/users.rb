212d6e54e42db81c9da73af3e5ad20464154fbac
DAVID MOORE <khundawg@gmail.com>
2015-01-20 21:13:22 -0500

    Clean up User factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,23 +4,21 @@ FactoryGirl.define do
     email "test@example.com"
     password "please123"
 
+    trait :admin do
+      role 'admin'
+    end
+    trait :with_routes do
+      after(:create) do |user|
+        FactoryGirl.create_list(:route, 5, user: user)
+      end
+    end
+
     factory :user_with_routes do
       transient do
         routes_count 5
       end
-
       after(:create) do |user, evaluator|
         create_list(:route, evaluator.routes_count, user: user)
-      end
-    end
-    
-    trait :admin do
-      role 'admin'
-    end
-
-    trait :with_routes do
-      after(:create) do |user|
-        FactoryGirl.create_list(:route, 5, user: user)
       end
     end
   end
